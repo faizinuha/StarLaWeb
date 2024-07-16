e7d89d820a7a9ab129af02bbfb88d60881aa4f97
MarDev <rozakadm@gmail.com>
2024-07-16 20:59:29 +0700

    update assets

--- layouts/navbar-templet.php
+++ layouts/navbar-templet.php
@@ -27,7 +27,7 @@ mysqli_close($koneksi);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Twiiter</title>
-    <link rel="icon" href="../assets/back_arrow_icon.png">
+    <link rel="icons" href="../asset/img/paimon-genshin-impact.avif">
     <!-- Bootstrap CSS -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <!-- Bootstrap Icons -->
